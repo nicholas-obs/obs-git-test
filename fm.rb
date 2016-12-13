72af2854951643a3488154417b92962c1f9b247f
Nicholas I <nicholas@oakridgebs.com>
2016-12-13 15:57:28 +0530

    added welcome function

--- fm.rb
+++ fm.rb
@@ -1,11 +1,11 @@
 puts "hi"
-<<<<<<< HEAD
-<<<<<<< HEAD
 puts "Nagaraj C"
 puts "test123"
-=======
 #Ramya
->>>>>>> 25e470f02dbb3acde52701c4c911fabe2bffcad0
-=======
 #this is a comment by vinutha
->>>>>>> ca9459a7a8b94c978733463181256a6256735445
+
+def welcome()
+    puts 'hello'
+end
+
+welcome()
